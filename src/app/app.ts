0645a508369cfe3940abd126cf21eaea7268acec
Mouad Hallaffou <mouadhallaffou@gmail.com>
2025-09-10 16:41:42 +0100

    update title signal in App component to reflect user name and message

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-angular-app');
+  protected readonly title = signal('mouad hallaffou, oh its my first ligne in angular, waw hhhh');
 }
